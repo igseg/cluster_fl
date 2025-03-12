48873373cd0124b48837ddc8357b04f2a256e062
igseg <ignacio.s.tamarit@gmail.com>
2025-03-12 14:37:03 -0700

    moved functions to tools

--- code/describe_1_day.py
+++ code/describe_1_day.py
@@ -2,63 +2,9 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from time import time
+from tools import *
 
 tardis_timestamp_to_dt = lambda x:  datetime.fromtimestamp(x/1e6)
-
-def tardis_times_to_dt(df, column):
-    df[column] = df[column].apply(tardis_timestamp_to_dt)
-    return df
-
-def filter_add_ttm(df, ttm_name='ttm', time_name='timestamp', maturity_name='expiration'):
-    """
-    adds time to maturity
-    Times must be as datetime objects
-    """
-    df[ttm_name] = df[maturity_name] - df[time_name]
-    return df
-
-def filter_0dte(df, ttm_name='ttm', days_to_maturity=1):
-    """
-    Times must be as datetime objects
-    """
-    fil = df[ttm_name].apply(lambda x:x.days < days_to_maturity)
-    return df[fil], df[~fil]
-    
-
-def filter_split_call_put(df, col_name='type', call_name='call', put_name='put'):
-    return df[df[col_name]==call_name], df[df[col_name]==put_name]
-
-def filter_2stds_dfcol(df, col_name):
-    mean = df[col_name].mean()
-    std = df[col_name].std()
-    up_limit = mean + 2 * std
-    low_limit = mean - 2 * std
-
-    condition = np.logical_and(df[col_name] < up_limit, df[col_name] < low_limit)
-    return df[condition]
-
-def load_tardis_data(file):
-    header = pd.read_csv(file, nrows=0)
-    column_names = header.columns.tolist()
-    n_cols = len(column_names)
-    columns_to_read = column_names[2:]
-
-    dtype = {}
-    for i in range(2,len(column_names)):
-        name = column_names[i]
-        if name in ['symbol', 'type', 'underlying_index']:
-            dtype[name] = 'category'
-        elif name in ['timestamp', 'local_timestamp']:
-            dtype[name] = 'int64'
-        elif name in ['expiration']:
-            continue
-        else:
-            dtype[name] = 'float32'
-    data_tardis = pd.read_csv(file,
-                          usecols=columns_to_read,
-                          dtype=dtype
-                         )
-    return data_tardis
 
 # Load Data
 
@@ -76,6 +22,7 @@ print(f'Time loading data: {(t_load - t0)/60:.2f} minutes')
 
 # Filters
 
+data_tardis = filter_coin_tardis(data_tardis)
 data_tardis = tardis_times_to_dt(data_tardis, 'timestamp')
 data_tardis = tardis_times_to_dt(data_tardis, 'expiration')
 data_tardis = filter_add_ttm(data_tardis)
